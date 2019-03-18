689090ac5325162c12b144d5c3c64c33702148bf
Amanda Treutler <amandathedev@gmail.com>
2019-03-18 17:10:40 -0500

    Changed examples

--- while_smiley.py
+++ while_smiley.py
@@ -1,13 +1,13 @@
 x = 1
 while x <= 10:
-    print("\U0001f600" * x)
+    print("\U0001f603" * x)
     x += 1
 
 # for loop version
 for num in range(1,11):
-    print("\U0001f600" * num)
+    print("\U0001f606" * num)
 
 # nested loop
 for x in range(3):
     for num in range(1,11):
-        print("\U0001f600" * num)
+        print("\U0001f602" * num)
