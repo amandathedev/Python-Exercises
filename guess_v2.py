a62c21da9ed98cdd2efb2259f5f1481fa38c3ad2
Amanda Treutler <amandathedev@gmail.com>
2019-03-19 19:19:54 -0500

    Improved guessing game v2

--- guess_v2.py
+++ guess_v2.py
@@ -1,13 +1,20 @@
 import random
 
 answer = random.randint(1,10)
-guess = input("Guess a number between 1 and 10: ")
 
-while answer > int(guess):
-    print("Too low, try again!")
-    print(f"The answer was {answer}!")
-    break
-while answer < int(guess):
-    print("Too high, try again!")
-    print(f"The answer was {answer}!")
-    break
+while True:
+    guess = input("Guess a number between 1 and 10: ")
+    guess = int(guess)
+    if guess < answer:
+        print("Too low, try again!")
+    elif guess > answer:
+        print("Too high, try again!")
+    else:
+        print("Congratulations! You won!")
+        play_again = input("Do you want to play again? (y/n) ")
+        if play_again == "y":
+            answer = random.randint(1,10)
+            guess = None
+        else:
+            print("Thank you for playing!")
+            break
