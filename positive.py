914f637163f955715565b04920a18e3a50c4b2bd
Amanda Treutler <amandathedev@gmail.com>
2019-03-24 09:41:25 -0500

    Even or odd and positive or negative numbers

---
+++ positive.py
@@ -0,0 +1,8 @@
+if x >0 and y > 0:
+    print("both positive")
+elif x < 0 and y < 0:
+    print("both negative")
+elif x > 0 and y < 0:
+    print("x is positive and y is negative")
+else:
+    print("y is positive and x is negative")
