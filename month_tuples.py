b2055f3094a5b1b196f61535f2975e04a02172c3
Amanda Treutler <amandathedev@gmail.com>
2019-04-04 18:12:52 -0500

    Tuple of months

---
+++ month_tuples.py
@@ -0,0 +1,6 @@
+months = ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+
+i = len(months) - 1
+while i >= 0:
+    print(months[i])
+    i -= 1
